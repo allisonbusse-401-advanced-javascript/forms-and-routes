4f5aed0cf0251b1c5b5e79865adaab8fa01fb022
allisonbusse <allisonbusse@gmail.com>
2019-11-04 22:40:10 -0800

    lots of functionality

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Home from '../containers/Home';
+import List from '../containers/List';
+import CharacterDetail from './CharacterDetail';
+
+export default function App() {
+  return (
+    <Router>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/list/:search" component={List} />
+          <Route exact path="/list" component={List} />
+          <Route path="/character/:name" component={CharacterDetail} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </>
+    </Router>
+  );
+}
---
+++ src/containers/Home.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Character from '../components/Character';
+import Search from '../components/Search';
+import PropTypes from 'prop-types';
+import styles from './Home.css';
+import { getCharacter } from '../services/api-call';
+
+
+class Home extends Component {
+
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  }
+
+  state = {
+    photo: '',
+    name: '',
+    search: '',
+  };
+
+  componentDidMount() {
+    getCharacter()
+      .then(character => {
+        this.setState({ photo: character[0].photoUrl, name: character[0].name });
+      });
+  }
+
+  handleClick = event => {
+    event.preventDefault();
+    getCharacter()
+      .then(character => {
+        this.setState({ photo: character[0].photoUrl, name: character[0].name });
+      });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const searchUrl = `/list/${this.state.search}`;
+    this.props.history.push(searchUrl);
+    
+
+
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ search: target.value });
+  }
+
+  render() {
+
+    return (
+      <div className={styles.Home}>
+        <Character photo={this.state.photo} name={this.state.name} handleClick={this.handleClick} />
+        <Search search={this.state.search} handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
+      </div>
+    );
+  }
+}
+
+export default Home;
+
